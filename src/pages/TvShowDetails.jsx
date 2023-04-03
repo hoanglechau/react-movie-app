42b474f3c083a2619704e4d9ad49aab4b13a44a5
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-04 03:24:53 +0700

    Create TvShowDetails.jsx

---
+++ src/pages/TvShowDetails.jsx
@@ -0,0 +1,42 @@
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import apiService from "../api/apiService";
+import { API_KEY } from "../api/config";
+import TDetailsCard from "../components/TDetailsCard";
+
+function TvShowDetails() {
+  let { tvId } = useParams();
+  const [loading, setLoading] = useState();
+  const [tvShowDetails, setTvShowDetails] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await apiService.get(
+          `tv/${tvId}?api_key=${API_KEY}&language=en-US&append_to_response=videos`
+        );
+        console.log(res.data);
+        setTvShowDetails(res.data);
+        setLoading(false);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    fetchData();
+  }, [tvId]);
+
+  return (
+    <>
+      <Typography variant="h5" mb={2}>
+        TV SHOW INFO
+      </Typography>
+      <Divider />
+
+      <TDetailsCard tvShowDetails={tvShowDetails} loading={loading} />
+    </>
+  );
+}
+
+export default TvShowDetails;
