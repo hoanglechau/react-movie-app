824031b438eee78128f54ddb3ef487f4894985f0
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-04 03:24:46 +0700

    Create PersonDetails.jsx

---
+++ src/pages/PersonDetails.jsx
@@ -0,0 +1,43 @@
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import apiService from "../api/apiService";
+import { API_KEY } from "../api/config";
+import PDetailsCard from "../components/PDetailsCard";
+
+function PersonDetails() {
+  let { personId } = useParams();
+  const [loading, setLoading] = useState();
+  const [personDetails, setpersonDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await apiService.get(
+          `person/${personId}?api_key=${API_KEY}&language=en-US`
+        );
+        console.log(res.data);
+        setpersonDetails(res.data);
+        setLoading(false);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    fetchData();
+  }, [personId]);
+
+  return (
+    <>
+      <Typography variant="h5" mb={2}>
+        PERSON INFO
+      </Typography>
+      <Divider />
+
+      <PDetailsCard personDetails={personDetails} loading={loading} />
+    </>
+  );
+}
+
+export default PersonDetails;
