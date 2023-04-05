8d10751bdaff55f2e1299f0bc4264c268ce9c6ae
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-06 02:52:36 +0700

    Update converters.js

--- src/utils/converters.js
+++ src/utils/converters.js
@@ -1,16 +1,16 @@
 // This file contains all the functions that convert the data from the API to a more readable format
 export const timeConvert = (time) => {
-  let hours = time / 60;
-  let roundedHours = Math.floor(hours);
-  let minutes = (hours - roundedHours) * 60;
-  let roundedMinutes = Math.round(minutes);
-  return roundedHours + "h " + roundedMinutes + "m";
+  const hours = time / 60;
+  const roundedHours = Math.floor(hours);
+  const minutes = (hours - roundedHours) * 60;
+  const roundedMinutes = Math.round(minutes);
+  return `${roundedHours}h ${roundedMinutes}m`;
 };
 
 export const dateConvert = (date) => {
-  let d = new Date(date);
-  let ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-  let mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
-  let da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
+  const d = new Date(date);
+  const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
+  const mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
+  const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
   return `${da} ${mo} ${ye}`;
 };
