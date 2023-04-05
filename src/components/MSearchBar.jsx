62aa10d580368f40cb01d1049c3efdeb3a7ef760
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-05 20:39:09 +0700

    Update MSearchBar.jsx

--- src/components/MSearchBar.jsx
+++ src/components/MSearchBar.jsx
@@ -14,7 +14,7 @@ const Search = styled("div")(({ theme }) => ({
   marginRight: theme.spacing(2),
   marginLeft: 0,
   width: "100%",
-  [theme.breakpoints.up("sm")]: {
+  [theme.breakpoints.up("xs")]: {
     marginLeft: theme.spacing(3),
     width: "auto"
   }
