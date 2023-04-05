c952cbcfa60868330cd36e95f71ca7385aec1738
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-05 19:35:43 +0700

    Update MSearchBar.jsx

--- src/components/MSearchBar.jsx
+++ src/components/MSearchBar.jsx
@@ -2,7 +2,6 @@ import SearchIcon from "@mui/icons-material/Search";
 import Box from "@mui/material/Box";
 import InputBase from "@mui/material/InputBase";
 import { alpha, styled } from "@mui/material/styles";
-import React from "react";
 import { useSearchParams } from "react-router-dom";
 
 const Search = styled("div")(({ theme }) => ({
@@ -10,15 +9,15 @@ const Search = styled("div")(({ theme }) => ({
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
   width: "100%",
   [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: "auto",
-  },
+    width: "auto"
+  }
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
@@ -28,21 +27,20 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
   pointerEvents: "none",
   display: "flex",
   alignItems: "center",
-  justifyContent: "center",
+  justifyContent: "center"
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "inherit",
   "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
+      width: "20ch"
+    }
+  }
 }));
 
 function MSearchBar() {
@@ -57,11 +55,7 @@ function MSearchBar() {
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{ flexGrow: { xs: 0.7, md: 0 } }}
-    >
+    <Box component="form" onSubmit={handleSubmit}>
       <Search>
         <SearchIconWrapper>
           <SearchIcon />
