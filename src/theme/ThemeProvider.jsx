2f469be946e302e9a51f2b6d5a88877bb410e1db
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-05 19:36:21 +0700

    Update ThemeProvider.jsx

--- src/theme/ThemeProvider.jsx
+++ src/theme/ThemeProvider.jsx
@@ -1,53 +1,52 @@
-import {
-  createTheme,
-  ThemeProvider as MUIThemeProvider,
-} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import darkScrollbar from "@mui/material/darkScrollbar";
+import {
+  createTheme,
+  ThemeProvider as MUIThemeProvider
+} from "@mui/material/styles";
 
 function ThemeProvider({ children }) {
   const themeOptions = {
     palette: {
-      mode: "light",
       primary: {
         light: "#757ce8",
         main: "#3f50b5",
         dark: "#002884",
-        contrastText: "#fff",
+        contrastText: "#fff"
       },
       secondary: {
         light: "#ff7961",
         main: "#f44336",
         dark: "#ba000d",
-        contrastText: "#000",
-      },
+        contrastText: "#000"
+      }
     },
     shape: { borderRadius: 8 },
     typography: {
       fontFamily: "'Work Sans', sans-serif",
       h5: {
         fontWeight: 800,
-        letterSpacing: 3,
+        letterSpacing: 3
       },
       h6: {
         fontSize: "28px",
-        fontWeight: 600,
+        fontWeight: 600
       },
       body: {
         fontSize: "18px",
-        fontWeight: 300,
+        fontWeight: 300
       },
       body1: {
-        fontWeight: 600,
-      },
+        fontWeight: 600
+      }
     },
     components: {
       MuiCssBaseline: {
         styleOverrides: (themeParam) => ({
-          body: themeParam.palette.mode === "dark" ? darkScrollbar() : null,
-        }),
-      },
-    },
+          body: darkScrollbar()
+        })
+      }
+    }
   };
 
   const theme = createTheme(themeOptions);
