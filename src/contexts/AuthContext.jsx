77bcfb53d7f74efca55e2dd7af73739dd9fae868
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-05 20:05:19 +0700

    Update AuthContext.jsx

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState } from "react";
 
+// This is the context that will be used to store the user data
 const AuthContextType = {
   user: "",
   signin: null,
@@ -7,6 +8,7 @@ const AuthContextType = {
 };
 const AuthContext = createContext(AuthContextType);
 
+// This component is used to provide the context to the children
 export function AuthProvider({ children }) {
   let [user, setUser] = useState("");
   let signin = (newUser, callback) => {
@@ -22,6 +24,7 @@ export function AuthProvider({ children }) {
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
+// This hook is used to get the context
 export function useAuth() {
   return useContext(AuthContext);
 }
